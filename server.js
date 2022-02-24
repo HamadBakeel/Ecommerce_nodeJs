ced21c6ad46910fe482bd7dd74673a9402b5971d
Hamad Bakeel <hamad200714@gmail.com>
2022-02-24 04:39:15 +0300

    added find product by id and show all category's products and showed all the dummyjson api products

--- server.js
+++ server.js
@@ -11,15 +11,60 @@ app.listen(PORT);
 
 
 app.get('/',(req,response)=>{
-    fetch('https://dummyjson.com/products?limit=30&skip=15&select=title,price,rating,discountPercentage')
-    .then(res => res.json())
-    .then(res => response.render('index', { Products: res.products }))
-    // .then(console.log);
-    // .then(response.render('index'))
-    // .then(res => res.json())
-    // .then(res => response.render('index'));
-    response.render("index")
+    fetch('https://dummyjson.com/products?limit=12&select=id,title,price,rating,discountPercentage,thumbnail')
+        .then(res => res.json())
+        .then(r1 =>
+            {fetch('https://dummyjson.com/products/categories')
+                .then(r2 => r2.json())
+                .then(r2 => response.render('index', { products: r1.products, categories: r2 }))
+        })
+    
 });
+
+app.get("/products?:category", (req, response) => {
+    var url = 'https://dummyjson.com/products';
+    if (!req.query.category)
+        url += '?select=title,price,rating,discountPercentage,thumbnail';
+
+    else
+        url += '/category/' + req.query.category;
+
+    fetch('https://dummyjson.com/products/categories')
+        .then(r2 => r2.json())
+        .then(r2 => {
+            fetch(url)
+                .then(res => res.json())
+                .then(res => response.render('categories', { products: res.products, categories: r2 }))
+        });
+})
+
+
+app.get("/products/:p_id?", (req, response) => {
+    if (req.params.p_id) {
+        fetch('https://dummyjson.com/products?limit=3&select=title,price,rating,discountPercentage,thumbnail')
+            .then(res1 => res1.json())
+            .then(r1 => {
+                fetch('https://dummyjson.com/products/categories')
+                    .then(r2 => r2.json())
+                    .then(r2 => {
+
+                        fetch('https://dummyjson.com/products/' + req.params.p_id)
+                            .then(res2 => res2.json())
+                            .then(r3 => response.render('product-details', { products: r1.products, categories: r2, product: r3, }))
+                    })
+            });
+    }
+});
+
+
+
+// app.get('/categories',(req,res)=>{
+//     fetch('https://dummyjson.com/products/categories')
+//                     .then(r2 => r2.json())
+//                     .then(r2 => res.render("categories",{categories: r2}));
+// })
+
+
 app.get('/index',(req,res)=>{
     res.render("index");
 })
@@ -31,10 +76,4 @@ app.get('/product-details',(req,res)=>{
 })
 app.get('/checkout',(req,res)=>{
     res.render("checkout");
-})
-app.get('/store',(req,res)=>{
-    res.render("store");
-})
-app.get('/categories',(req,res)=>{
-    res.render("store");
 })
